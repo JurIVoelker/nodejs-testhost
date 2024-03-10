7399e60ff1d9c4da51c4e81c65171dddae4d95ff
JurIVoelker <Juri.Voelker@gmx.de>
2024-03-10 16:20:43 +0100

    no-local-host

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -1,4 +1,4 @@
-const host = "http://localhost:3000";
+const host = "https://nodejs-testhost.vercel.app/";
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 window.onscroll = function () {
