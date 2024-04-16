fc006800857eaf8c9e975c0ce65b2b70312390a6
JurIVoelker <Juri.Voelker@gmx.de>
2024-04-16 20:52:36 +0200

    testing...

--- scripts/pageModify.js
+++ scripts/pageModify.js
@@ -2,6 +2,25 @@ const fs = require("fs");
 const path = require("path");
 const log = require('node-file-logger');
 
+const options = {
+  timeZone: 'America/Los_Angeles',
+  folderPath: path.join(__dirname, "..", "logs") + "/",      
+  dateBasedFileNaming: true,
+  // Required only if dateBasedFileNaming is set to false
+  fileName: 'All_Logs',   
+  // Required only if dateBasedFileNaming is set to true
+  fileNamePrefix: 'Logs_',
+  fileNameSuffix: '',
+  fileNameExtension: '.log',     
+  
+  dateFormat: 'YYYY-MM-DD',
+  timeFormat: 'HH:mm:ss.SSS',
+  logLevel: 'debug',
+  onlyFileLogging: true
+}
+
+log.SetUserOptions(options);
+
 class PageModify {
   static loadPage(filePath, callback) {
     fs.readFile(filePath, "utf8", (err, data) => {
